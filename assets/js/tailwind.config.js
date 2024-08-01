b92daf37c49fcc4d6a9acff5c8e5af1b859adb55
vishnubishnoi0371 <vishnukhicher49@gmail.com>
2024-08-01 16:20:30 +0530

    first commit

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,39 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          sky: "#a0d9f4",
+          skin: "#ff9267",
+          pink: "#ff5d92",
+          yellow: "#ffdc00",
+          purple: "#935bf3",
+          grey: "#595959",
+          "light-grey": "#f5f5f5",
+          "primary-grey": "#e3e3e3",
+        },
+        fontSize: {
+          'custom-3xl': '32px',
+          "custom-5xl": "64px",
+        },
+        fontFamily: {
+          plusJakarta: ["Plus Jakarta Sans", "sans-serif"],
+        },
+        lineHeight: {
+          100: "100%",
+          109: "109%",
+          110: "110%",
+          125: "125%",
+          126: "126%",
+          133: "133%",
+          140: "140%",
+          150: "150%",
+          171: "171%",
+          200: "200%",
+        },
+        screen: {
+          xs: "400px",
+        },
+      },
+    },
+  };
+  
